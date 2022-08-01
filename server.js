8ed47406b246f7e3ea14d00c32ddb7ae1e51b55c
Yenphuong Tong <yenphuongtong@Yenphuongs-MacBook-Pro.local>
2022-08-01 11:27:32 -0700

    added controllers

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+const bandsController = require('./controllers/bands_controller')
+const stagesController = require('./controllers/stages_controllers')
+const eventsController = require('./controllers/events_controller')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
+app.use('/bands', bandsController)
+app.use('/stages', stagesController)
+app.use('/events', eventsController)
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
